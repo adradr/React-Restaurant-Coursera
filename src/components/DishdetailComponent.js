3c89ab274aab5d341fc43ab287f3d1a70ba77660
adr <adrian.lenard@me.com>
2019-08-25 20:57:07 +0200

    Task 1,2,3 ready

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -13,7 +13,7 @@ class DishDetail extends Component {
                 <Card>
                     <CardImg top src={dish.image} alt={dish.name} />
                     <CardBody>
-                      <CardTitle><span class="badge badge-dark">{dish.name}</span></CardTitle>
+                      <CardTitle><span className="badge badge-dark">{dish.name}</span></CardTitle>
                       <CardText>{dish.description}</CardText>
                     </CardBody>
                 </Card>
@@ -25,14 +25,19 @@ class DishDetail extends Component {
     }
 
     renderComments(dish){
-        const comments = this.props.dish.map((comment) => {
-            return (
-                <div key={comment.comments.id}>
-                    valami
-                </div>
-            );
-        });
-        if (dish != null)
+        
+        if (dish != null){
+            const comments = this.props.dish.comments.map((comment) => {
+                return (
+                    <div>
+                        <ul key={comment.id} className="list-unstyled">
+                            <li>{comment.comment}</li>
+                            <li>-- {comment.author}, {comment.date}</li>
+                        </ul>
+                    </div>
+                );
+            });
+
             return(
                 <div>
                     <Card>
@@ -41,10 +46,12 @@ class DishDetail extends Component {
                     </Card>
                 </div>
             );
-        else
+        } else {
             return(
                 <div></div>
             );
+        }
+            
     }
 
     render() {
@@ -52,10 +59,10 @@ class DishDetail extends Component {
         return (
             <div className="container">
                 <div className="row">
-                    <div  className="col-12 col-md-5 m-1">
+                    <div className="col-12 col-md-5 m-1">
                         {this.renderDish(selected)}
                     </div>
-                    <div  className="col-12 col-md-5 m-1">
+                    <div className="col-12 col-md-5 m-1">
                         {this.renderComments(selected)}
                     </div>
                 </div>
